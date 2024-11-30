d6c0207b81d79d044a8854eca4a073370a7b79de
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-11-30 21:56:25 +0100

    added post processing  related to health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,18 +6,19 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] public int _currentHealth;
     [SerializeField] public int _maxHealth;
-    [SerializeField] private float _healthDecreaseInterval = 1.0f; 
-    [SerializeField] private int _healthDecreaseAmount = 1; 
+    [SerializeField] private float _healthDecreaseInterval = 1.0f;
+    [SerializeField] private int _healthDecreaseAmount = 1;
     private float _timeSinceLastDecrease;
-    
-    
+    private PostProcessingManager _postProcessingManager;
+
     private void Start()
     {
         _currentHealth = _maxHealth;
         _timeSinceLastDecrease = 0f;
+        _postProcessingManager = FindObjectOfType<PostProcessingManager>();
+        UpdatePostProcessing();
     }
-    
-   
+
     private void Update()
     {
         _timeSinceLastDecrease += Time.deltaTime;
@@ -27,21 +28,20 @@ public class PlayerHealth : MonoBehaviour
             DecreaseHealth(_healthDecreaseAmount);
             _timeSinceLastDecrease = 0f;
         }
-       
     }
-    
-    
+
     private void DecreaseHealth(int amount)
     {
         _currentHealth -= amount;
         if (_currentHealth <= 0)
         {
             _currentHealth = 0;
-
             Destroy(gameObject);
-           
         }
+
+        UpdatePostProcessing();
     }
+
     public void Heal(int healAmount)
     {
         _currentHealth += healAmount;
@@ -49,6 +49,21 @@ public class PlayerHealth : MonoBehaviour
         {
             _currentHealth = _maxHealth;
         }
+
+        UpdatePostProcessing();
     }
 
+    private void UpdatePostProcessing()
+    {
+        if (_postProcessingManager != null)
+        {
+            float healthPercentage = (float)_currentHealth / _maxHealth;
+            Debug.Log($"Updating post-processing with health percentage: {healthPercentage}");
+            _postProcessingManager.UpdateEffects(healthPercentage);
+        }
+        else
+        {
+            Debug.LogError("PostProcessingManager is not assigned.");
+        }
+    }
 }
