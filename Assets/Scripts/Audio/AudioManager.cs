2fedcdf31c7916c666ec0eff539ffcf918f48c43
Cyranixonpj <163138757+Cyranixonpj@users.noreply.github.com>
2024-11-30 20:11:33 +0100

    Music , bullet enemy

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    [Header("-----------AUDIO SOURCES-----------")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+    
+    [Header("-----------AUDIO CLIPS-----------")]
+    public AudioClip backgroundMusic;
+    public AudioClip death;
+    public AudioClip Shoot;
+    public AudioClip Hit;
+    public AudioClip Walk;
+    
+    void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+        
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
