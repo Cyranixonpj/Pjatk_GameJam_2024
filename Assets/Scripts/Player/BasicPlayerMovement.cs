571ac5d3ed5e9bd2b833cce98440d3f3d79c89ab
fz3002 <franc3002@gmail.com>
2024-11-30 16:05:02 +0100

    Add Basic Player Movement
    # Conflicts:
    #	.idea/.idea.Pjatk_GameJam_2024/.idea/.gitignore

---
+++ Assets/Scripts/Player/BasicPlayerMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class BasicPlayerMovement : MonoBehaviour
+    {
+
+        [SerializeField]private float speed;
+        private float _speedX, _speedY, _xInput, _yInput;
+        Rigidbody2D _rigidbody;
+        
+        private Vector2 _moveDirection, _mousePosition;
+        private Camera _camera;
+
+        void Awake()
+        {
+            _camera = Camera.main;
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            //TODO:Check if moving diagonally makes u move faster
+            _xInput = Input.GetAxisRaw("Horizontal");
+            _yInput = Input.GetAxisRaw("Vertical");
+            _moveDirection = new Vector2(_xInput, _yInput).normalized;
+            if (_camera) _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        private void FixedUpdate()
+        {
+            _speedX = _xInput * speed;
+            _speedY = _yInput * speed;
+            _rigidbody.velocity = new Vector2(_speedX, _speedY);
+            Vector2 aimDirection = _mousePosition - _rigidbody.position;
+            float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+            _rigidbody.rotation = aimAngle;
+        }
+    }
+}
