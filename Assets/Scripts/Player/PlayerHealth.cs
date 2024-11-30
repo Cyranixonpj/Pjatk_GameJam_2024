9901f42439a652b9b01060e2ec9982e960b9ceab
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-11-30 18:22:42 +0100

    sanity and health HUD

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private int _currentHealth;
-    [SerializeField] private int _maxHealth;
+    [SerializeField] public int _currentHealth;
+    [SerializeField] public int _maxHealth;
     [SerializeField] private float _healthDecreaseInterval = 1.0f; 
     [SerializeField] private int _healthDecreaseAmount = 1; 
     private float _timeSinceLastDecrease;
