c4a9d9c31be7b971702dd3cd21ccfefb5a4ce7a7
fz3002 <franc3002@gmail.com>
2024-11-30 17:06:50 +0100

    Add player shooting

---
+++ Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] bulletPrefabs;
+        [SerializeField] private float bulletSpeed;
+        [SerializeField] private Transform firePoint;
+        [SerializeField] private float fireCooldown;
+        private float _fireTimer;
+
+        public void Update()
+        {
+            _fireTimer -= Time.deltaTime;
+        }
+
+        public void Fire()
+        {
+            if (_fireTimer <= 0)
+            {
+                Debug.Log("Fire");
+                _fireTimer = fireCooldown;
+                GameObject bullet = bulletPrefabs[FindProjectile()];
+                bullet.transform.position = firePoint.position;
+                bullet.GetComponent<Bullet>().Activate();
+                bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+            }
+        }
+
+        private int FindProjectile()
+        {
+            for (var i = 0; i < bulletPrefabs.Length; i++)
+                if (!bulletPrefabs[i].activeInHierarchy)
+                    return i;
+            return 0;
+        }
+    }
+}
