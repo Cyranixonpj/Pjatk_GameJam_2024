75bb8fda0d1a49302a847bcb17fa8efc2881669c
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-11-30 17:12:13 +0100

    player health basic system/ apap prefab

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int _currentHealth;
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private float _healthDecreaseInterval = 1.0f; 
+    [SerializeField] private int _healthDecreaseAmount = 1; 
+    private float _timeSinceLastDecrease;
+    
+    
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+        _timeSinceLastDecrease = 0f;
+    }
+    
+   
+    private void Update()
+    {
+        _timeSinceLastDecrease += Time.deltaTime;
+
+        if (_timeSinceLastDecrease >= _healthDecreaseInterval)
+        {
+            DecreaseHealth(_healthDecreaseAmount);
+            _timeSinceLastDecrease = 0f;
+        }
+       
+    }
+    
+    
+    private void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+          
+           
+        }
+    }
+    public void Heal(int healAmount)
+    {
+        _currentHealth += healAmount;
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+    }
+
+}
